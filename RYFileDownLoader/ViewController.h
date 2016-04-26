5294c9033cffe4fcac4330d5130b90cedde329f7
2252819957 <wei.wutao@rongyu100.com>
2016-04-26 10:05:18 +0800

    文件创建

---
+++ RYFileDownLoader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RYFileDownLoader
+//
+//  Created by wwt on 16/4/26.
+//  Copyright © 2016年 rongyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RYFileDownLoader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RYFileDownLoader
+//
+//  Created by wwt on 16/4/26.
+//  Copyright © 2016年 rongyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
